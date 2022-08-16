d0b7fdb44857aec19f83b10d086ab0aec8ebc4d8
Ahmedkorya682 <111289577+Ahmedkorya682@users.noreply.github.com>
2022-08-17 00:55:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+/*let x=[1,2,3,4,5,6]
+
+
+let y=x.map((i)=>{
+
+return i
+
+
+})
+console.log(y);
+
+let z=x.filter((i)=>{
+
+    return i>3
+    
+    
+    })
+    console.log(z);
+
+
+    let e=x.find((i)=>{
+
+        return i>3
+        
+        
+        })
+        console.log(e);
+
+        let korya=(x=[])=>{
+
+       let y=   x.filter((i)=>{
+return i%2==0
+          })
+          return y
+        }
+
+        let r=korya(x)
+        console.log(r)*/
+
+      /*  function test(){
+return new Promise((resolve,reject)=>{
+
+  setTimeout(()=>{
+
+    console.log("test");
+    resolve()
+  },5000)
+})
+
+
+
+        }
+
+        
+        function test2(){
+          return new Promise((resolve,reject)=>{
+          
+            setTimeout(()=>{
+          
+              console.log("test2");
+              resolve()
+            },3000)
+          })
+          
+          
+          
+                  }
+
+                  function test3(){
+
+
+                    setTimeout(()=>{
+                      console.log("test3")
+                    },1000)
+                  }
+
+
+                  async function korya(){
+
+await test()
+await test2()
+test3()
+
+
+                  }
+
+                  korya()*/
